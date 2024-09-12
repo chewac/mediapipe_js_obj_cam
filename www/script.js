5e5d17dde1ff9138104c99d9c7b187f4c5b68147
Kirill Piddubko <38920548+chewac@users.noreply.github.com>
2024-09-13 02:06:42 +0400

    Update script.js

--- www/script.js
+++ www/script.js
@@ -136,7 +136,23 @@ async function enableCam(event) {
     enableWebcamButton.classList.add("removed");
     // getUsermedia parameters
     const constraints = {
-        video:true
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
+    facingMode: {
+      exact: 'environment'
+    }
+  }
+}
+        
     };
     // Activate the webcam stream.
     navigator.mediaDevices
